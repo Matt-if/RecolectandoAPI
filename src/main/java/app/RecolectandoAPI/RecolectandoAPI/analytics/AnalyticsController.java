d9cc74225fd5da68927020098214de6b5fe7812d
Matias F <matiasfavorito@gmail.com>
2025-07-14 17:44:11 -0300

    control de errores en tipo de residuo enviado en analytics

--- src/main/java/app/RecolectandoAPI/RecolectandoAPI/analytics/AnalyticsController.java
+++ src/main/java/app/RecolectandoAPI/RecolectandoAPI/analytics/AnalyticsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,16 +25,27 @@ public class AnalyticsController {
     //id 1 = query weight-wasteType-by-sector
     // disadvantage is that the requester should know RetrievalType enum values
     @GetMapping("/weight-wasteType-by-sector/{type}")
-    public ResponseEntity<ApiResponse> weightOfWasteTypeBySector(@PathVariable RetrievalType type) {
-        List<DTO> results = analyticsService.weightOfWasteTypeBySector(type).stream()
-                .map(ToDTO::analyticsResult)
-                .collect(Collectors.toList());
+    public ResponseEntity<ApiResponse> weightOfWasteTypeBySector(@PathVariable final String type) {
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.builder()
-                        .data(results)
-                        .msg("Datos obtenidos con exito!")
-                        .build());
+        try {
+            if (!List.of(RetrievalType.values()).toString().contains(type))
+                throw new IllegalArgumentException("Tipo de residuo no existente: " + type );
 
+            List<DTO> results = analyticsService.weightOfWasteTypeBySector(RetrievalType.valueOf(type)).stream()
+                    .map(ToDTO::analyticsResult)
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.builder()
+                            .data(results)
+                            .msg("Datos obtenidos con exito!")
+                            .build());
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.builder()
+                            .msg(e.getMessage())
+                            .build());
+        }
     }
 }
