0dea3c8ef4d06729fad911a8589211b74b657454
Matias F <matiasfavorito@gmail.com>
2025-07-15 15:19:35 -0300

    implementacion de Logout

--- src/main/java/app/RecolectandoAPI/RecolectandoAPI/config/SecurityConfig_Prod.java
+++ src/main/java/app/RecolectandoAPI/RecolectandoAPI/config/SecurityConfig_Prod.java
@@ -1,11 +1,17 @@
 package app.RecolectandoAPI.RecolectandoAPI.config;
 
+import app.RecolectandoAPI.RecolectandoAPI.entities.token.Token;
+import app.RecolectandoAPI.RecolectandoAPI.entities.token.TokenRepo;
+import app.RecolectandoAPI.RecolectandoAPI.entities.user.User;
 import app.RecolectandoAPI.RecolectandoAPI.errorHandling.JwtAuthenticationEntryPoint;
 import app.RecolectandoAPI.RecolectandoAPI.jwt.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -13,8 +19,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +36,7 @@ public class SecurityConfig_Prod {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authProvider;
     private final JwtAuthenticationEntryPoint jwtAuthEntryPoint;
+    private final TokenRepo tokenRepo;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
@@ -44,10 +56,52 @@ public class SecurityConfig_Prod {
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/auth/logout")
+                                .addLogoutHandler(this::logout)
+                                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+                )
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(jwtAuthEntryPoint)
                 )
                 .build();
+    }
+
+    // Si el usuario cierra sesion, el objetivo es que el refresh token que tenia asociado ya no sea valido.
+    private void logout(
+            final HttpServletRequest request,
+            final HttpServletResponse response,
+            final Authentication authentication
+    ) {
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7);
+        final Token storedToken = tokenRepo.findByToken(jwt);
+        if (storedToken != null) {
+            revokeAllUserTokens(storedToken);
+        }
+        else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    private void revokeAllUserTokens(Token t) {
+        Long id = tokenRepo.findUser_IdByToken(t);
+
+        List<Token> validUserTokens = tokenRepo.findAllExpiredIsFalseOrRevokedIsFalseByUserId(id);
+
+        if (!validUserTokens.isEmpty()) {
+            for (Token token : validUserTokens) {
+                token.setRevoked(true);
+                token.setExpired(true);
+            }
+            tokenRepo.saveAll(validUserTokens);
+        }
     }
 
 }
--- src/main/java/app/RecolectandoAPI/RecolectandoAPI/entities/token/TokenRepo.java
+++ src/main/java/app/RecolectandoAPI/RecolectandoAPI/entities/token/TokenRepo.java
@@ -1,13 +1,16 @@
 package app.RecolectandoAPI.RecolectandoAPI.entities.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TokenRepo extends JpaRepository<Token, Long> {
     Token findByToken(String token);
 
-    List<Token> findAllValidIsFalseOrRevokedIsFalseByUserId(Long userId);
-
     List<Token> findAllExpiredIsFalseOrRevokedIsFalseByUserId(Long id);
+
+    @Query("select t.user.id from Token t where t = :token")
+    Long findUser_IdByToken(@Param("token") Token t);
 }
