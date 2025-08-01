172d32693fe33919da65e046889c590f7e361615
Matias F <matiasfavorito@gmail.com>
2025-08-01 16:59:25 -0300

    refactoring minimo por nombre de clase

--- src/main/java/app/RecolectandoAPI/RecolectandoAPI/entities/analytics/AnalyticsMapper.java
+++ src/main/java/app/RecolectandoAPI/RecolectandoAPI/entities/analytics/AnalyticsMapper.java
@@ -1,4 +1,4 @@
-package app.RecolectandoAPI.RecolectandoAPI.analytics;
+package app.RecolectandoAPI.RecolectandoAPI.entities.analytics;
 
 import org.springframework.stereotype.Service;
 
---
+++ src/main/java/app/RecolectandoAPI/RecolectandoAPI/entities/analytics/AnalyticsRepo.java
@@ -0,0 +1,7 @@
+package app.RecolectandoAPI.RecolectandoAPI.entities.analytics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalyticsRepo extends JpaRepository<Analytics, Long> {
+
+}
